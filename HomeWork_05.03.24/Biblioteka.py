da77731de4f30c7f1b1b79a9a8f89617f62fc98f
HamzaDmitriyevich <krou1937@gmail.com>
2024-03-05 16:24:37 +0300

    сделал задания 4 и 3

--- HomeWork_05.03.24/Biblioteka.py
+++ HomeWork_05.03.24/Biblioteka.py
@@ -1,3 +1,12 @@
 def put_v_library():
     question = input('How to get to the library')
-    
+    return question
+
+
+vyzov = put_v_library()
+rasdel = vyzov.split()[0]
+print(rasdel)
+
+
+
+#1. Спросить человека маршрут ("как пройти в библиотеку"). Получить ответ в виде строки. Сохранить в переменную. Вывести первое слово ответа на экран
---
+++ HomeWork_05.03.24/pustoepole.py
@@ -0,0 +1,11 @@
+width = 10
+height = 5
+
+top_bottom_border = "#" * width
+center = "#" + " " * (width - 2) + "#"
+
+print(top_bottom_border)
+print(center)
+print(center)
+print(center)
+print(top_bottom_border)
